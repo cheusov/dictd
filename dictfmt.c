88590759e2071aa00e5c4edd32011da4bf031423
Aleksey Cheusov <vle@gmx.net>
2003-12-30 16:35:16 +0000

    *** empty log message ***

--- dictfmt.c
+++ dictfmt.c
@@ -17,7 +17,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 675 Mass Ave, Cambridge, MA 02139, USA.
  * 
- * $Id: dictfmt.c,v 1.36 2003/12/30 16:08:13 cheusov Exp $
+ * $Id: dictfmt.c,v 1.37 2003/12/30 16:35:16 cheusov Exp $
  *
  * Sun Jul 5 18:48:33 1998: added patches for Gutenberg's '1995 CIA World
  * Factbook' from David Frey <david@eos.lugs.ch>.
@@ -507,7 +507,6 @@ static void fmt_newheadword( const char *word )
    end = ftell(str);
 
    if (word && !without_hw){
-      fprintf (stderr, "headword: %s\n", word);
       fmt_string (word);
       fmt_newline();
    }
